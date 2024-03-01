d50ac0cae06ec3218e9b941c6f2a188a44724b61
Malwina9086 <malwina.lukjanska1@gmail.com>
2024-03-01 21:12:49 +0000

    Improve email sending functionality
    - Introduce sendVerificationEmail function to handle email sending with recipient email and verification token as parameters.
    - Move SendGrid API key configuration to the main function for better modularity.
    - Replace static email content (recipient email, subject, etc.) with dynamic values fetched from environment variables.
    - Utilize process.env.SENDGRID_EMAIL_USER and process.env.SENDGRID_API_KEY as values for sender and recipient, enhancing flexibility and security.

--- services/emailService.js
+++ services/emailService.js
@@ -1,17 +1,33 @@
 const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-const msg = {
-  to: "malwina@gmail.com",
-  from: "malwina@gmail.com",
-  subject: "Sending with SendGrid is Fun",
-  text: "and easy to do anywhere, even with Node.js",
-  html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+
+const sendVerificationEmail = () => {
+  const recipientEmail = process.env.SENDGRID_EMAIL_USER;
+  const verificationToken = process.env.SENDGRID_API_KEY;
+
+  if (!recipientEmail || !verificationToken) {
+    console.error(
+      "Recipient email or verification token not provided in environment variables."
+    );
+    return;
+  }
+
+  const msg = {
+    from: process.env.SENDGRID_EMAIL_USER,
+    to: recipientEmail,
+    subject: "Verification Email",
+    text: `Click the following link to verify your email: http://localhost:3000/users/verify/${verificationToken}`,
+  };
+
+  return sgMail.send(msg);
 };
-sgMail
-  .send(msg)
+
+sendVerificationEmail()
   .then(() => {
     console.log("Email sent");
   })
   .catch((error) => {
     console.error(error);
   });
+
+module.exports = sendVerificationEmail;
